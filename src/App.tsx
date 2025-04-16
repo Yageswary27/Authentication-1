c0cc7f159f9effbcd2ecc28e92acac8f1f75e899
yageswary27 <yageswaryyadav04@gmail.com>
2025-04-16 17:09:59 +0545

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./components/Auth/LoginForm";
-import SignupForm from "./components/Auth/SignupForm";
-import ForgotPassword from "./components/Auth/ForgotPassword";
+// import LoginForm from "./components/Auth/LoginForm";
+// import SignupForm from "./components/Auth/SignupForm";
+// import ForgotPassword from "./components/Auth/ForgotPassword";
+import Usersetting from "./pages/Usersetting";
+
 
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
+       <Routes>
+        {/*
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        <Route path="/user-settings" element={<Usersetting/>} />
         
       </Routes>
     </Router>
