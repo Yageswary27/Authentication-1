f06a3163a7647f1d7b3289abc96644e82be965e0
yageswary27 <yageswaryyadav04@gmail.com>
2025-04-16 17:11:28 +0545

    usersetting update

---
+++ src/pages/Usersetting.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserProfile from '../components/usersettings/UserProfiles';
+
+const Usersetting: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <UserProfile />
+    </div>
+  );
+};
+
+export default Usersetting;
