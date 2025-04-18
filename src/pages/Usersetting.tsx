020c186f66da061444d1f785a3e35ac1eb6f6ef8
yageswary27 <yageswaryyadav04@gmail.com>
2025-04-18 09:23:27 +0545

    updated

--- src/pages/Usersetting.tsx
+++ src/pages/Usersetting.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import UserProfile from '../components/usersettings/UserProfiles';
+import BillingDetails from '../components/usersettings/BillingDetails';
+import BillingHistory from '../components/BillingHistorys/BillingHistorys';
 
 const Usersetting: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-x-4 min-h-screen bg-gray-100">
+      <div>
       <UserProfile />
+      </div>
+      <div>
+      <BillingHistory />
+      </div>
+        <div>
+        <BillingDetails />
+        </div>
     </div>
   );
 };
