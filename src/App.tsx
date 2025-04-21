259472b422e84d33272551e5e52d04af5121ea2f
yageswary27 <yageswaryyadav04@gmail.com>
2025-04-21 09:13:29 +0545

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LoginForm from "./components/Auth/LoginForm";
-// import SignupForm from "./components/Auth/SignupForm";
-// import ForgotPassword from "./components/Auth/ForgotPassword";
+ import LoginForm from "./components/Auth/LoginForm";   
+ import SignupForm from "./components/Auth/SignupForm";
+ import ForgotPassword from "./components/Auth/ForgotPassword";
 import Usersetting from "./pages/Usersetting";
 import BillingDetails from "./components/usersettings/BillingDetails";
 import BillingHistory from "./components/BillingHistorys/BillingHistorys";
@@ -14,10 +14,10 @@ const App: React.FC = () => {
   return (
     <Router>
        <Routes>
-        {/*
+        
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        <Route path="/forgot-password" element={<ForgotPassword />} /> 
         <Route path="/user-settings" element={<Usersetting/>} />
         <Route path="/billing-details" element={<BillingDetails/>} />
         <Route path="/billing-history" element={<BillingHistory/>} />
